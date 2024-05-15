f97caac5bc2ebc854a8f99fee3db41b34c46a4a2
Yocmar Rafael  Jiménez González <yocmarjigon@gmail.com>
2024-05-15 15:07:41 -0500

    Solición a error con la conexión a base de datos

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-import fs from 'node:fs';
+
 const datasource = new Sequelize({
   host: "dpg-cp0371njbltc73dmjl2g-a.oregon-postgres.render.com",
   port: 5432,
@@ -21,10 +21,5 @@ const connection =  () => {
     datasource.authenticate().then(() => console.log("Connection has been established successfully.")).catch((error) => console.error("Unable to connect to the database:", error)).catch((error) => console.error("Unable to connect to the database:", error));
 }
 
-const generateTable = () => {
-   sql` CREATE TABLE IF NOT EXISTS admin (id uuid default gen_random_uuid() primary key , email varchar not null unique,password varchar not null);`.then(() => console.log("done"));
-   sql`CREATE TABLE IF NOT EXISTS empleador (id uuid default gen_random_uuid() primary key , nombre varchar not null, direccion varchar not null, telefono varchar not null, correo varchar not null, fechaContratacion date not null, departamento varchar not null, salario int not null, puesto varchar not null);`
-
-};
 
 export { connection, generateTable };
