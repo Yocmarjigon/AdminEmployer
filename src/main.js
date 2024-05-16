ff5654402382ce230defe048660c896fe5e30766
Yocmar Rafael  Jiménez González <yocmarjigon@gmail.com>
2024-05-16 16:26:02 -0500

    Solución de error al hacer las consultas a la DB

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ app.use("/admin", routerAdmin);
 app.use("/empleados", routerEmpleado);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("<h1>Hello World!</h1>");
 });
 
 app.use((err, req, res, next) => {
